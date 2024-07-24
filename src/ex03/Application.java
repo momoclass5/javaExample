8924fa538b4b2e5435ac3cd67da6fab17e09f2b6
오미선 <momo.class5@gmail.com>
2024-07-24 15:14:10 +0900

    정렬

--- src/ex03/Application.java
+++ src/ex03/Application.java
@@ -2,12 +2,13 @@ package ex03;
 
 import ex03.practice.NonStaticSample;
 
-public class Application {
+public class Application extends Object {
     public static void main(String[] args) {
         NonStaticSample nss = new NonStaticSample();
 
         // 반환타입이 없는 메서드는 출력할 수 없음
         // 실행하고 끝!!!!!!
         nss.printLottoNumbers();
+        System.out.println(nss.toString());
     }
 }
--- src/ex03/practice/NonStaticSample.java
+++ src/ex03/practice/NonStaticSample.java
@@ -10,33 +10,52 @@ public class NonStaticSample {
         // i= 0 ~ 5 6번 실행
         for (int i = 0; i < lotto.length; i++) {
             lotto[i] = (int) (Math.random() * 45) + 1;
-
+            for (int j = 0; j < i; j++) {
+                if (lotto[j] == lotto[i]) {
+                    i--;
+                    break;
+                }
+            }
         }
         System.out.println(Arrays.toString(lotto));
 
         // 응용 --> 오름차순 정렬로 출력
         // int[] sortLotto = sort(lotto);
-        sort(lotto);
+        sort(lotto, false); // Arrays.sort(lotto);
         System.out.println(Arrays.toString(lotto));
+
     }
 
     /**
      * int[]을 매개변수로 받아서 정렬후 정렬된 int[]을 반환
      * 
+     * boolean타입의 매개변수를 받아서
+     * true : 오름차순
+     * false : 내림차순
+     * 
      * @param lotto
      * @return
      */
     // private int[] sort(int[] lotto) {
-    private void sort(int[] lotto) {
+    private void sort(int[] lotto, boolean sort) {
         // 정렬 i=기준이 되는 방
         for (int i = 0; i < lotto.length; i++) {
             // j = 비교대상
             for (int j = i + 1; j < lotto.length; j++) {
-                if (lotto[i] > lotto[j]) {
-                    int temp = lotto[i];
-                    lotto[i] = lotto[j];
-                    lotto[j] = temp;
+                if (sort) {
+                    if (lotto[i] > lotto[j]) {
+                        int temp = lotto[i];
+                        lotto[i] = lotto[j];
+                        lotto[j] = temp;
+                    }
+                } else {
+                    if (lotto[i] < lotto[j]) {
+                        int temp = lotto[i];
+                        lotto[i] = lotto[j];
+                        lotto[j] = temp;
+                    }
                 }
+
             }
         }
         // return lotto;
