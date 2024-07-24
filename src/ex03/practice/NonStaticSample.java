9f6d0a6a292adf66304aa1ee177a337514771f19
오미선 <momo.class5@gmail.com>
2024-07-24 14:44:30 +0900

    정렬

--- src/ex03/practice/NonStaticSample.java
+++ src/ex03/practice/NonStaticSample.java
@@ -3,17 +3,43 @@ package ex03.practice;
 import java.util.Arrays;
 
 public class NonStaticSample {
+
     public void printLottoNumbers() {
         // 1~45까지의 임의의 정수 6개가 중복되지 않게 발생시켜 출력하는 메소드
         int[] lotto = new int[6];
         // i= 0 ~ 5 6번 실행
         for (int i = 0; i < lotto.length; i++) {
             lotto[i] = (int) (Math.random() * 45) + 1;
+
         }
         System.out.println(Arrays.toString(lotto));
 
         // 응용 --> 오름차순 정렬로 출력
+        // int[] sortLotto = sort(lotto);
+        sort(lotto);
+        System.out.println(Arrays.toString(lotto));
+    }
 
+    /**
+     * int[]을 매개변수로 받아서 정렬후 정렬된 int[]을 반환
+     * 
+     * @param lotto
+     * @return
+     */
+    // private int[] sort(int[] lotto) {
+    private void sort(int[] lotto) {
+        // 정렬 i=기준이 되는 방
+        for (int i = 0; i < lotto.length; i++) {
+            // j = 비교대상
+            for (int j = i + 1; j < lotto.length; j++) {
+                if (lotto[i] > lotto[j]) {
+                    int temp = lotto[i];
+                    lotto[i] = lotto[j];
+                    lotto[j] = temp;
+                }
+            }
+        }
+        // return lotto;
     }
 
     public void outputChar(int num, char c) {
