87d0e15967ff89d4423e0e96e83b87fd4cfbfb39
오미선 <momo.class5@gmail.com>
2024-08-02 11:57:53 +0900

    테이블 생성쿼리

--- SQL/실습_도서관리.sql
+++ SQL/실습_도서관리.sql
@@ -100,8 +100,27 @@ COMMENT ON COLUMN TB_BOOK.REGDATE IS '등록일자';
 --           PHONE(연락처)       
 --           DELYN(탈퇴 여부)     -- 기본값으로 'N' 그리고 'Y' 혹은 'N'으로 입력되도록 제약조건
 --           REGDATE(가입일)  -- 기본값으로 SYSDATE, NOT NULL
+CREATE TABLE TB_MEMBER(
+    M_NO            CHAR(6)         PRIMARY KEY
+    , ID            VARCHAR2(20)    UNIQUE
+    , PW            VARCHAR2(20)    NOT NULL
+    , NAME          VARCHAR2(20)    NOT NULL
+    , GENDER        CHAR(1)         CHECK (GENDER IN ('F', 'M'))
+    , ADDR          VARCHAR2(100)
+    , PHONE         VARCHAR2(20)
+    , DELYN         CHAR(1)         DEFAULT('N') CHECK (DELYN IN ('Y','N'))
+    , REGDATE       DATE            DEFAULT SYSDATE
+
+);
 
 --  2) 3개 정도의 샘플 데이터 추가하기
+INSERT INTO TB_MEMBER (M_NO, ID, PW, NAME) VALUES ('M00001', 'USER01', 'USER01', '지디');
+INSERT INTO TB_MEMBER (M_NO, ID, PW, NAME) VALUES ('M00002', 'USER02', 'USER02', '아몰랑');
+INSERT INTO TB_MEMBER (M_NO, ID, PW, NAME) VALUES ('M00003', 'USER03', 'USER03', '오늘도');
+INSERT INTO TB_MEMBER (M_NO, ID, PW, NAME) VALUES ('M00004', 'USER04', 'USER04', '홍형경');
+INSERT INTO TB_MEMBER (M_NO, ID, PW, NAME) VALUES ('M00005', 'USER05', 'USER05', '어제도');
+INSERT INTO TB_MEMBER (M_NO, ID, PW, NAME) VALUES ('M00006', 'USER06', 'USER06', '박서현');
+SELECT * FROM TB_MEMBER;
 
 
 -- 4. 도서를 대여한 회원에 대한 데이터를 담기 위한 대여 목록 테이블(TB_RENT)
@@ -112,9 +131,73 @@ COMMENT ON COLUMN TB_BOOK.REGDATE IS '등록일자';
 --                                      이때 부모 데이터 삭제 시 NULL 값이 되도록 옵션 설정
 --           RENT_DATE(대여일) -- 기본값 SYSDATE
 --           RETURN_DATE(반납일)
+CREATE TABLE TB_RENT(
+    R_NO            CHAR(6)     PRIMARY KEY
+    , M_NO          CHAR(6)     REFERENCES TB_MEMBER(M_NO)
+    , B_NO          CHAR(6)     REFERENCES TB_BOOK(B_NO)
+    , RENT_DATE     DATE        DEFAULT SYSDATE
+    , RETURN_DATE   DATE
+);
 
+SELECT * FROM TB_MEMBER;
+SELECT * FROM TB_BOOK;
 
 -- 아이디가 M00001번인 사람이 B00003번책을 대여
+INSERT INTO TB_RENT (R_NO, M_NO, B_NO) 
+            VALUES ('R00001', 'M00001', 'B00001');
+
+-- 테이블의 데이터를 수정
+UPDATE TB_BOOK 
+SET RENTYN='Y', R_NO='R00001'--, ... 
+WHERE B_NO = 'B00001';
+-------------------------------------
+--도서 대여
+-------------------------------------
+-- 아이디가 M00002인 사람이 B00002번 책을 대여
+-- 1. 책의 상태를 확인
+SELECT RENTYN FROM TB_BOOK WHERE B_NO = 'B00002';
+-- 2. RENT 테이블에 대여 이력을 입력
+INSERT INTO TB_RENT (R_NO, M_NO, B_NO) VALUES ('R00002', 'M00002', 'B00002');
+-- 3. BOOK 테이블의 대여 여부를 업데이트
+UPDATE TB_BOOK SET RENTYN = 'Y' WHERE B_NO ='B00002';
+
+-------------------------------------
+--도서 반납
+-------------------------------------
+-- 1. 책의 상태를 확인
+SELECT RENTYN FROM TB_BOOK WHERE B_NO = 'B00002';
+-- RENT 테이블의 반납일을 업데이트
+
+-- 도서테이블에 대여번호컬럼을 추가 합니다.
+ALTER TABLE TB_BOOK ADD R_NO CHAR(6) REFERENCES TB_RENT(R_NO);
+SELECT R_NO FROM TB_BOOK WHERE B_NO = 'B00002';
+UPDATE TB_BOOK SET R_NO = 'R00002' WHERE B_NO='B00002';
+
+
+SELECT * FROM TB_RENT WHERE B_NO = 'B00002' AND RETURN_DATE IS NULL;
+-- BOOK 테이블의 대여 여부 업데이트
+UPDATE TB_RENT 
+SET RETURN_DATE = SYSDATE 
+WHERE B_NO = 'B00002' AND RETURN_DATE IS NULL;
+
+SELECT * FROM TB_BOOK;
+UPDATE TB_BOOK
+SET RENTYN = 'N' 
+WHERE B_NO = 'B00002';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- 렌트번호, 회원번호, 도서번호
